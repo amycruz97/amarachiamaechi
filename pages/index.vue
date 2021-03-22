4733729acef4b4305413a4e0e9ac2efcca115208
amy <amarachiamaechi4@gmail.com>
2021-03-22 22:50:43 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,77 @@
+<template>
+  <nav class="relative flex flex-wrap items-center justify-between py-3 navbar-expand-lg bg-red-500 mb-3 px-20">
+    <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
+      <div class="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
+        <a class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-no-wrap uppercase text-white" href="#pablo">
+          AMY
+        </a>
+        <button class="text-white bg-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none" type="button" v-on:click="toggleNavbar()">
+          <font-awesome-icon :icon="{ prefix: 'fas', iconName: 'fa-bars' }" style="color: #4267B2; font-size: 72px;"/>
+          <i class="fas fa-bars"></i>
+        </button>
+      </div>
+      <div v-bind:class="{'hidden': !showMenu, 'flex': showMenu}" class="lg:flex lg:flex-grow items-center">
+        <ul class="flex flex-col lg:flex-row list-none ml-auto">
+          <li class="nav-item">
+            <a class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
+              <i class="fab fa-facebook-square text-lg leading-lg text-white opacity-75" /><span class="ml-2">About</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
+              <i class="fab fa-twitter text-lg leading-lg text-white opacity-75" /><span class="ml-2">Writing</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
+              <i class="fab fa-pinterest text-lg leading-lg text-white opacity-75" /><span class="ml-2">Speaking</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
+              <i class="fab fa-pinterest text-lg leading-lg text-white opacity-75" /><span class="ml-2">Contact</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "pink-navbar",
+  data() {
+    return{
+ showMenu: false
+    }
+     
+    
+  },
+  methods: {
+    toggleNavbar: function(){
+      this.showMenu = !this.showMenu;
+    }
+  }
+}
+</script>
+
+
+
+
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+body{
+  background-color: #15202B;
+
+
+}
+
+
+
+</style>
