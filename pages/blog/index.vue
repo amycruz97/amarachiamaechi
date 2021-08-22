e60501df2585eb11fdb5ac751616729450b99cd3
Tosho Ajibade <toshoajibade@gmail.com>
2021-08-22 22:55:57 +0100

    Nuxt content articles not showing on blog index
    The blog markdown was not properly formatted therefore the markdown is invalid
    
    Remove the empty lines at the top of the markdown, the --- is suppose to be the first thing on the page

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,28 +1,29 @@
 <template>
- <div>
-  <div class="lg:px-20 px-10 pb-32">
- 
-      <h3 class="text-purple font-bold text-6xl mt-16 mb-20 text-center">Writing</h3>
+  <div>
+    <div class="lg:px-20 px-10 pb-32">
+      <h3 class="text-purple font-bold text-6xl mt-16 mb-20 text-center">
+        Writing
+      </h3>
 
-    <div class="flex justify-between  my-5">
-      <div>
-        <svg
-          width="34"
-          height="40"
-          viewBox="0 0 34 39"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M19.2188 0.415771H14.4177V29.4442L3.31965 18.3462L0.0327148 21.67L16.8182 38.4555L33.6407 21.67L30.2799 18.3462L19.2188 29.4442V0.415771Z"
-            fill="#fff"
-          />
-        </svg>
+      <div class="flex justify-between  my-5">
+        <div>
+          <svg
+            width="34"
+            height="40"
+            viewBox="0 0 34 39"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M19.2188 0.415771H14.4177V29.4442L3.31965 18.3462L0.0327148 21.67L16.8182 38.4555L33.6407 21.67L30.2799 18.3462L19.2188 29.4442V0.415771Z"
+              fill="#fff"
+            />
+          </svg>
+        </div>
+        <div class=" text-white font-bold pr-5 mt-2">100 Articles</div>
       </div>
-      <div class=" text-white font-bold pr-5 mt-2">100 Articles</div>
-    </div>
 
-    <!-- <div class="flex lg:flex-row flex-col justify-between lg:px-4 mb-10 mt-10">
+      <!-- <div class="flex lg:flex-row flex-col justify-between lg:px-4 mb-10 mt-10">
      
      <div class="lg:w-5/12 w-full  shadow-3xl mb-20 border-b-2 border-purple" id="article">
        <img src="@/assets/images/cover.png" alt="">
@@ -38,65 +39,47 @@
      
     </div> -->
 
- <ul class="bg-red-400">
-      <li v-for="article of articles" :key="article.slug">
-        <NuxtLink :to="{ name: 'blog-slug', params: { slug: article.slug } }">
-          <img :src="article.img" />
-          <div>
-            <h2>{{ article.title }}</h2> 
-            <!-- <p>by {{ article.author.name }}</p> -->
-             <p>{{ article.description }}</p>
-          </div>
-        </NuxtLink>
-      </li>
-    </ul>
-       
+      <ul class="bg-red-400">
+        <li v-for="article of articles" :key="article.slug">
+          <NuxtLink :to="{ name: 'blog-slug', params: { slug: article.slug } }">
+            <img :src="article.img" />
+            <div>
+              <h2>{{ article.title }}</h2>
+              <!-- <p>by {{ article.author.name }}</p> -->
+              <p>{{ article.description }}</p>
+            </div>
+          </NuxtLink>
+        </li>
+      </ul>
 
-       <!-- <div class="flex lg:flex-row flex-col justify-between lg:px-4 mb-10 mt-10">
+      <!-- <div class="flex lg:flex-row flex-col justify-between lg:px-4 mb-10 mt-10">
          <div :key="article.slug">
          <PostCard :item="article"/>
          </div>
 
        </div> -->
-
-
-
-  
-
-  </div>
- <Newsletter/>
- 
-
+    </div>
+    <Newsletter />
   </div>
 </template>
 
 <script>
 import Newsletter from "@/components/Newsletter";
 export default {
-    components:{
-        Newsletter
-    },
+  components: {
+    Newsletter
+  },
 
-     async asyncData({ $content, params }) {
-      const articles = await $content('articles')
-        // .only(['title', 'description', 'img', 'slug', 'author'])
-        // .sortBy('createdAt', 'asc')
-        // .fetch()
-
-        .where({ published: { $ne: false } })
-        .sortBy('date', 'desc')
-        .fetch()
-
-        console.log(articles[0].body)
-
-      return {
-        articles
-      }
-    },
-    
+  async asyncData({ $content, params }) {
+    const articles = await $content("articles")
+      .only(["title", "description", "img", "slug", "author"])
+      .sortBy("createdAt", "asc")
+      .fetch()
+    return {
+      articles
+    };
+  }
 };
 </script>
 
-<style  scoped>
-
-</style>
+<style scoped></style>
