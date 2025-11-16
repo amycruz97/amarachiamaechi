401d82579e7095e3fc369332cf4e37fc2f48c971
Amarachi Amaechi <31285686+amycruz97@users.noreply.github.com>
2025-11-15 17:13:39 -0800

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,70 +9,65 @@ export default {
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: 'Amarachi Amaechi website' },
-      {hid: 'description', name: 'image', href: '/icon.png' },
+      { hid: 'description', name: 'image', href: '/icon.png' },
       { hid: 'og:description', name: 'og:description', content: 'Amarachi Amaechi website' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-       {
-        rel: "stylesheet",
-        href: "https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&family=Roboto+Slab:wght@100;300;400;500;600;700&display=swap"
-      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&family=Roboto+Slab:wght@100;300;400;500;600;700&display=swap'
+      }
     ]
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
+  // Global CSS
   css: [
     '~/assets/css/style.css',
     '@fortawesome/fontawesome-svg-core/styles.css'
   ],
 
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  // Plugins
   plugins: [
     '~/plugins/fontawesome.js'
   ],
 
-  // Auto import components: https://go.nuxtjs.dev/config-components
+  // Auto import components
   components: true,
 
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  // Modules for dev and build
   buildModules: [
-    // https://go.nuxtjs.dev/tailwindcss
     '@nuxtjs/tailwindcss',
     '@nuxtjs/fontawesome',
     '@nuxtjs/color-mode'
   ],
 
-  // Modules: https://go.nuxtjs.dev/config-modules
+  // Nuxt modules
   modules: [
-    // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
-    // https://go.nuxtjs.dev/pwa
     '@nuxtjs/pwa',
-    // https://go.nuxtjs.dev/content
-    '@nuxt/content',
+    '@nuxt/content'
   ],
 
-  // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
 
-  // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
       lang: 'en'
     }
   },
 
-  // Content module configuration: https://go.nuxtjs.dev/config-content
   content: {
-  markdown: {
-    prism: {
-      theme: 'prism-themes/themes/prism-material-oceanic.css'
+    markdown: {
+      prism: {
+        theme: 'prism-themes/themes/prism-material-oceanic.css'
+      }
     }
-  }
-},
+  },
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
+  // Build Configuration
   build: {
+    // Turn off cssnano / PostCSS optimization that is crashing
+    optimizeCSS: false
   }
 }
