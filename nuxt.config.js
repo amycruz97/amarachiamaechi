3b9c4cc8a3d795cd342422fe3b81400e9c0ddde7
Amarachi Amaechi <31285686+amycruz97@users.noreply.github.com>
2025-11-15 17:18:19 -0800

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,47 +16,51 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
         rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&family=Roboto+Slab:wght@100;300;400;500;600;700&display=swap'
+        href:
+          'https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&family=Roboto+Slab:wght@100;300;400;500;600;700&display=swap'
       }
     ]
   },
 
-  // Global CSS
+  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     '~/assets/css/style.css',
     '@fortawesome/fontawesome-svg-core/styles.css'
   ],
 
-  // Plugins
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
     '~/plugins/fontawesome.js'
   ],
 
-  // Auto import components
+  // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
-  // Modules for dev and build
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     '@nuxtjs/tailwindcss',
     '@nuxtjs/fontawesome',
     '@nuxtjs/color-mode'
   ],
 
-  // Nuxt modules
+  // Modules: https://go.nuxtjs.dev/config-modules
   modules: [
     '@nuxtjs/axios',
     '@nuxtjs/pwa',
     '@nuxt/content'
   ],
 
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
 
+  // PWA module configuration: https://go.nuxtjs.dev/config-pwa
   pwa: {
     manifest: {
       lang: 'en'
     }
   },
 
+  // Content module configuration: https://go.nuxtjs.dev/config-content
   content: {
     markdown: {
       prism: {
@@ -65,9 +69,14 @@ export default {
     }
   },
 
-  // Build Configuration
+  // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
     // Turn off cssnano / PostCSS optimization that is crashing
-    optimizeCSS: false
+    optimizeCSS: false,
+    postcss: {
+      plugins: {
+        cssnano: false
+      }
+    }
   }
 }
